f4faa3f775a60022df26e46e5fdc75feff774424
Angela Donati <angeladonati93@gmail.com>
2022-06-22 12:26:23 -0400

    add comments

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -5,23 +5,25 @@ AWS.config.update({
   });
 
   const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
-
+// params contains table name and required schema
   const params = {
     TableName: 'Thoughts',
     KeySchema: [
       { AttributeName: 'username', KeyType: 'HASH' }, // Partition key
       { AttributeName: 'createdAt', KeyType: 'RANGE' }, // Sort key
     ],
+    // define attributes for hash and range
     AttributeDefinitions: [
       { AttributeName: 'username', AttributeType: 'S' },
       { AttributeName: 'createdAt', AttributeType: 'N' },
     ],
+    // maximum read and write capacity
     ProvisionedThroughput: {
       ReadCapacityUnits: 10,
       WriteCapacityUnits: 10,
     },
   };
-
+//   create table in dynamodb using defined params
   dynamodb.createTable(params, (err, data) => {
     if (err) {
       console.error(
--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -7,7 +7,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
 
 console.log("Importing thoughts into DynamoDB. Please wait.");
 const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
-
+// iterate over each user to implement into the db
 allUsers.forEach(user => {
     const params = {
       TableName: "Thoughts",
